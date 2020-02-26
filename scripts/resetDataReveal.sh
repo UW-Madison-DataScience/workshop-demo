38dec3902772917b6fd6fa9e5fea8d46d8c2be2a
Sarah Stevens <sarah.stevens@wisc.edu>
2020-02-26 11:58:36 -0600

    updated data reset

--- scripts/resetDataReveal.sh
+++ scripts/resetDataReveal.sh
@@ -2,5 +2,5 @@
 
 cp clean_data/2020-02-12-portal-data-joined.csv clean_data/portal_data_joined.csv
 now=$(date +"%Y-%m-%d")
-rm clean_data/${now}-portal_data_joined.csv 
+rm clean_data/${now}-portal_data_joined.csv clean_data/2020-02-12-portal_data_joined.csv
 rm raw_data/${now}-messydata_v7.csv
