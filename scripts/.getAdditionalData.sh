69388006ebc4704ababa85fed227de3979b1e5a5
Sarah Stevens <sarah.stevens@wisc.edu>
2022-06-20 13:43:06 -0500

    updating data date

--- scripts/.getAdditionalData.sh
+++ scripts/.getAdditionalData.sh
@@ -1,6 +1,6 @@
 # Bash script to unhide the additional data sent by the fictitious collaborator
 
-cp clean_data/portal_data_joined.csv clean_data/2020-02-12-portal_data_joined.csv
+cp clean_data/portal_data_joined.csv clean_data/2022-02-12-portal_data_joined.csv
 now=$(date +"%Y-%m-%d")
 cp clean_data/.portal_data_joined_2000.csv clean_data/${now}-portal_data_joined.csv 
 cp clean_data/.portal_data_joined.csv clean_data/portal_data_joined.csv
